a08e673c3614d9a02dded4dd11a600d52f113bfa
Alpenglow88 <igoddard77@gmail.com>
2018-06-29 16:26:19 +0100

    rubocop checks on report_builder

--- report_builder.rb
+++ report_builder.rb
@@ -1,12 +1,12 @@
 require 'report_builder'
 time = Time.now.getutc
 ReportBuilder.configure do |config|
- config.json_path = 'testreport.json'
- config.report_path = 'cucumber_web_report'
- config.report_types = [:html]
- config.report_tabs = %w[Overview Features Scenarios Errors]
- config.report_title = 'Cucumber web automation test results'
- config.compress_images = false
- config.additional_info = { 'Project name' => 'UI', 'Platform' => 'Integration', 'Report generated' => time }
+  config.json_path = 'testreport.json'
+  config.report_path = 'cucumber_web_report'
+  config.report_types = [:html]
+  config.report_tabs = %w[Overview Features Scenarios Errors]
+  config.report_title = 'Cucumber web automation test results'
+  config.compress_images = false
+  config.additional_info = { 'Project name' => 'UI', 'Platform' => 'Integration', 'Report generated' => time }
 end
 ReportBuilder.build_report
