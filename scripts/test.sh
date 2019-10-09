d9defe8da6c1f69395af739859a5750d04ba9ec8
Alpenglow88 <igoddard77@gmail.com>
2019-10-09 15:12:26 +0100

    fixing run script errors

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,12 @@
-# export HOST="" # URL of testing
-# export USERNAME="" # Username for login if required
-# export PASSWORD="" # Password for login if required
-export OPTIONS="-t\\@test" # Options to test. Needs to be in format '-t\\@<TAG>' 
-export DRIVER="selenium_chrome" # Driver to be used, list can be found '...features/support/browser_drivers.rb'
-# export OUTPUT_FILE="test_report"
-# export OUTPUT_FILE_TITLE="Test Report"
-# export RERUN_OUTPUT_FILE="retest_report"
-# export RERUN_OUTPUT_FILE_TITLE="Rerun Test Report"
+export HOST="" # URL of testing
+export USERNAME="" # Username for login if required
+export PASSWORD="" # Password for login if required
+export OPTIONS="" # Options to test. Needs to be in format '-t\\@<TAG>' 
+export DRIVER="" # Driver to be used, list can be found '...features/support/browser_drivers.rb'
+export OUTPUT_FILE="test_report"
+export OUTPUT_FILE_TITLE="Test Report"
+export RERUN_OUTPUT_FILE="retest_report"
+export RERUN_OUTPUT_FILE_TITLE="Rerun Test Report"
 
 PLUGIN_LOCAL_PATH="/temp"
 PLUGIN_USERNAME=$USERNAME
@@ -19,19 +19,5 @@ PLUGIN_OUTPUT_FILE_TITLE=$OUTPUT_FILE_TITLE
 PLAYER_SANDBOX_PATH="..."
 
 rake test
-# rake rerun
-# rake report
-
-# docker run \
-#     --volume $<path>:$PLUGIN_LOCAL_PATH \
-#     -e PLUGIN_LOCAL_PATH \
-#     -e PLUGIN_USERNAME \
-#     -e PLUGIN_PASSWORD_TESTING \
-#     -e PLUGIN_PASSWORD_STAGING \
-#     -e PLUGIN_HOST \
-#     -e PLUGIN_OPTIONS \
-#     -e PLUGIN_DRIVER \
-#     -e PLUGIN_OUTPUT_FILE \
-#     -e PLUGIN_OUTPUT_FILE_TITLE \
-#     -it <name>/<name>:1 \
-#     /bin/sh
+rake rerun
+rake report
