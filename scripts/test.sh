0761d3ad4815a55277c7d2ba1e09f8ad0330b088
Alpenglow88 <igoddard77@gmail.com>
2019-10-09 15:22:50 +0100

    adding info about blanck OPTIONS

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,12 @@
-# export HOST="" # URL of testing
-# export USERNAME="" # Username for login if required
-# export PASSWORD="" # Password for login if required
+export HOST="" # URL of testing
+export USERNAME="" # Username for login if required
+export PASSWORD="" # Password for login if required
 export OPTIONS="" # Options to test. Needs to be in format '-t\\@<TAG>' 
-export DRIVER="selenium_chrome" # Driver to be used, list can be found '...features/support/browser_drivers.rb'
-# export OUTPUT_FILE="test_report"
-# export OUTPUT_FILE_TITLE="Test Report"
-# export RERUN_OUTPUT_FILE="retest_report"
-# export RERUN_OUTPUT_FILE_TITLE="Rerun Test Report"
+export DRIVER="" # Driver to be used, list can be found '...features/support/browser_drivers.rb'
+export OUTPUT_FILE="test_report"
+export OUTPUT_FILE_TITLE="Test Report"
+export RERUN_OUTPUT_FILE="retest_report"
+export RERUN_OUTPUT_FILE_TITLE="Rerun Test Report"
 
 PLUGIN_LOCAL_PATH="/temp"
 PLUGIN_USERNAME=$USERNAME
@@ -19,8 +19,8 @@ PLUGIN_OUTPUT_FILE_TITLE=$OUTPUT_FILE_TITLE
 PLAYER_SANDBOX_PATH="..."
 
 rake test
-# rake rerun
-# rake report
+rake rerun
+rake report
 
 # docker run \
 #     --volume $<path>:$PLUGIN_LOCAL_PATH \
