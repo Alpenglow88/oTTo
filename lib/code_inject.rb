bb8f8b46169f8cffef660b8f29d46980e6f0236b
Alpenglow88 <ian.goddard@theculturetrip.com>
2020-03-26 12:15:47 +0000

    adding text

--- lib/code_inject.rb
+++ lib/code_inject.rb
@@ -2,6 +2,6 @@
 
 # This file can be used to organise any code for injection into the page under test that isnt native Ruby
 
-# Inject into page using page.execute_script(script)
+# Inject into page using page.execute_script(script) where script is your CODE_CONSTANT below
 
 CODE_CONSTANT = '<code>'
