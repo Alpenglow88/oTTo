2723c1a5e6bfe2fdafd1a28ec941c1f64213c62b
Alpenglow88 <igoddard77@gmail.com>
2021-06-24 14:36:52 +0100

    fixing retest report builder

--- lib/report_builder.rb
+++ lib/report_builder.rb
@@ -18,14 +18,12 @@ end
 ReportBuilder.build_report
 
 ReportBuilder.configure do |config|
-  if File.exist?('./retest.report.json')
-    config.json_path = 'retestreport.json'
-    config.report_path = "#{RERUN_OUTPUT_FILE}_#{time}"
-    config.report_types = [:html]
-    config.report_tabs = %w[Overview Features Scenarios Errors]
-    config.report_title = "#{RERUN_OUTPUT_FILE_TITLE}_#{time}"
-    config.compress_images = false
-    config.additional_info = { 'Project name': 'UI_Automation', Platform: 'Player', 'Report generated': time }
-  end
-  ReportBuilder.build_report
+  config.json_path = 'retestreport.json'
+  config.report_path = "#{RERUN_OUTPUT_FILE}_#{time}"
+  config.report_types = [:html]
+  config.report_tabs = %w[Overview Features Scenarios Errors]
+  config.report_title = "#{RERUN_OUTPUT_FILE_TITLE}_#{time}"
+  config.compress_images = false
+  config.additional_info = { 'Project name': 'UI_Automation', Platform: 'Player', 'Report generated': time }
 end
+ReportBuilder.build_report if File.exist?('./retestreport.json')
