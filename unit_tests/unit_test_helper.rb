e3556048b81c289a0ec0a9bcc4ac53e8739b7aec
Alpenglow88 <igoddard77@gmail.com>
2021-06-25 11:47:52 +0100

    updating to latest gem version

--- unit_tests/unit_test_helper.rb
+++ unit_tests/unit_test_helper.rb
@@ -7,7 +7,7 @@ require './unit_tests/tests/create_data_test_id'
 require './unit_tests/tests/percent_of'
 require './unit_tests/tests/percent_divide_to_float'
 require './unit_tests/tests/total_time_split'
-require './unit_tests/tests/language_detection'
+# require './unit_tests/tests/language_detection'
 require './unit_tests/tests/timecode_split'
 
 # Files under test
